2de3dc385102c1455e33550e8677e051e39e229d
12TheHangedMan/TLGCohort <106269882+12TheHangedMan@users.noreply.github.com>
2023-01-31 09:31:55 -0800

    Add files via upload

---
+++ src/main/java/GameMap/SubArea.java
@@ -0,0 +1,12 @@
+package Game_Map;
+
+public class SubArea {
+
+    private String map_name = "Laboratory";
+
+    private Content contents;
+
+    public void initial_subArea() {
+        this.contents = new Content();
+    }
+}
