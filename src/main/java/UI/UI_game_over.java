3570df143d878ef9860aa2d6ca997b06de66d344
12TheHangedMan/TLGCohort <106269882+12TheHangedMan@users.noreply.github.com>
2023-01-31 09:33:39 -0800

    Add files via upload

---
+++ src/main/java/UI/UI_game_over.java
@@ -0,0 +1,7 @@
+package UI;
+
+public class UI_game_over {
+    public static void load_game_over() {
+        System.out.println("Game Over");
+    }
+}
---
+++ src/main/java/UI/UI_main_screen.java
@@ -0,0 +1,8 @@
+package UI;
+
+public class UI_main_screen {
+    public static void load_main_screen() {
+        System.out.println("General Becker: " +
+                "Your mission is to collect a blood sample from this area.");
+    }
+}
