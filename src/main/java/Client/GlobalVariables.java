c940cfa4f02ec128b4d1efd4436a3920e0b27da4
12TheHangedMan/TLGCohort <106269882+12TheHangedMan@users.noreply.github.com>
2023-01-31 09:34:52 -0800

    Add files via upload

---
+++ src/main/java/Client/GlobalVariables.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+
+public class GlobalVariables {
+
+    private int exit_code = 0;
+
+    public int getExitCode() {
+        return exit_code;
+    }
+
+    public void setExitCode(int exit_code) {
+        this.exit_code = exit_code;
+    }
+
+    public static void gameInitialization() {
+
+
+    }
+}
