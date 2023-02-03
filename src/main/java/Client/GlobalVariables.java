26472820ede373545ad5c1ab58bbce04c5b1176c
EthanW <wyqgerman@gmail.com>
2023-02-03 12:29:26 -0500

    AB#200

--- src/main/java/Client/GlobalVariables.java
+++ src/main/java/Client/GlobalVariables.java
@@ -4,6 +4,17 @@ import java.util.Map;
 
 public class GlobalVariables {
 
+    public static void gameExitMessage() {
+        System.out.println("Thank you for playing StrainX!");
+    }
+
+    public static void playerHelpCall() {
+        System.out.println("Here are some tips:");
+        System.out.println("You can interact with items, by typing get [item] or use [item].");
+        System.out.println("You can move from your location to another location by typing go [direction].");
+        System.out.println("Examples: go north or use shotgun");
+    }
+
     private int exit_code = 0;
 
     public int getExitCode() {
