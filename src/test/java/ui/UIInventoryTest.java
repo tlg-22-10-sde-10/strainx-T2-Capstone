beb27adce249e2d0d2f464fc5f7bfe1e0dcdf7ed
12TheHangedMan/TLGCohort <chongwei.ma@outlook.com>
2023-02-09 11:23:11 -0800

    AB#226
    player can read a mini map

--- src/test/java/ui/UIInventoryTest.java
+++ src/test/java/ui/UIInventoryTest.java
@@ -28,55 +28,55 @@ public class UIInventoryTest {
         }
     }
 
-    @Test
-    public void displayInventoryListTest() throws IOException {
-        GlobalVariables.gameInitialization();
-
-        for(int i=0; i<10; i++) {
-            var item = ItemFactory.createItem();
-
-            pickUpItem(item);
-        }
-    }
-
-    @Test
-    public void useItemsTest() throws IOException {
-        GlobalVariables.gameInitialization();
-
-        var weaponName = "AR-15";
-        var medicalsName = "bandages";
-        var keyItemsName = "body armor";
-
-        var weapon = ItemFactory.createItem();
-        var medical1 = ItemFactory.createItem();
-        var medical2 = ItemFactory.createItem();
-        var key = ItemFactory.createItem();
-
-        pickUpItem(weapon);
-        pickUpItem(medical1);
-        pickUpItem(medical2);
-        pickUpItem(key);
-
-        for (var k : InventoryMap.keySet()) {
-            System.out.println(InventoryMap.get(k).getName() + " x" + InventoryMap.get(k).getQty());
-        }
-        System.out.println("----------");
-
-        System.out.println(GlobalVariables.mySquad.get(0).getWeapon().getName());
-        System.out.println("----------");
-        System.out.println("use Items");
-
-        useItems(weaponName);
-        useItems(medicalsName);
-        useItems(medicalsName);
-        useItems(keyItemsName);
-
-        for (var k : InventoryMap.keySet()) {
-            System.out.println(InventoryMap.get(k).getName() + " x" + InventoryMap.get(k).getQty());
-        }
-        System.out.println("----------");
-
-        System.out.println(GlobalVariables.mySquad.get(0).getWeapon().getName());
-        System.out.println("----------");
-    }
+//    @Test
+//    public void displayInventoryListTest() throws IOException {
+//        GlobalVariables.gameInitialization();
+//
+//        for(int i=0; i<10; i++) {
+//            var item = ItemFactory.createItem();
+//
+//            pickUpItem(item);
+//        }
+//    }
+//
+//    @Test
+//    public void useItemsTest() throws IOException {
+//        GlobalVariables.gameInitialization();
+//
+//        var weaponName = "AR-15";
+//        var medicalsName = "bandages";
+//        var keyItemsName = "body armor";
+//
+//        var weapon = ItemFactory.createItem();
+//        var medical1 = ItemFactory.createItem();
+//        var medical2 = ItemFactory.createItem();
+//        var key = ItemFactory.createItem();
+//
+//        pickUpItem(weapon);
+//        pickUpItem(medical1);
+//        pickUpItem(medical2);
+//        pickUpItem(key);
+//
+//        for (var k : InventoryMap.keySet()) {
+//            System.out.println(InventoryMap.get(k).getName() + " x" + InventoryMap.get(k).getQty());
+//        }
+//        System.out.println("----------");
+//
+//        System.out.println(GlobalVariables.mySquad.get(0).getWeapon().getName());
+//        System.out.println("----------");
+//        System.out.println("use Items");
+//
+//        useItems(weaponName);
+//        useItems(medicalsName);
+//        useItems(medicalsName);
+//        useItems(keyItemsName);
+//
+//        for (var k : InventoryMap.keySet()) {
+//            System.out.println(InventoryMap.get(k).getName() + " x" + InventoryMap.get(k).getQty());
+//        }
+//        System.out.println("----------");
+//
+//        System.out.println(GlobalVariables.mySquad.get(0).getWeapon().getName());
+//        System.out.println("----------");
+//    }
 }
