9474029f03c01137ce4f34bfddae9beb9a77b0a4
antoni <antoni.dev0@gmail.com>
2023-02-16 10:33:41 -0800

    a player can click on a button and a new panel will open to show the subarea, players can exit the subarea and return to the main map, AB#1169

--- src/main/java/ui/gui/GUIEntry.java
+++ src/main/java/ui/gui/GUIEntry.java
@@ -3,6 +3,9 @@ package ui.gui;
 import gamecontrol.GameDifficulty;
 import gamecontrol.GlobalVariables;
 import gamemodel.mapengine.MainMap;
+import gamemusic.AudioPlayer;
+import ui.gui.components.MapPanel;
+import ui.gui.components.StatusPanel;
 import ui.gui.components.WrapperPanel;
 
 import javax.swing.*;
@@ -12,19 +15,19 @@ import java.io.IOException;
 public class GUIEntry {
     public static void start() throws IOException {
 
-        GlobalVariables.inGameMap = new MainMap(GameDifficulty.Easy);
+        GlobalVariables.inGameMap = new MainMap(GameDifficulty.Medium);
         GlobalVariables.gameInitialization();
 
-        // TEST StatsPanel
+        WrapperPanel wrapperPanel = new WrapperPanel();
+
+        // TEST
         JFrame jFrame = new JFrame();
-
-        jFrame.add(new WrapperPanel());
-
         jFrame.setResizable(false);
         jFrame.setTitle("StrainX");
         jFrame.setPreferredSize(new Dimension(1024,768));
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.pack();
         jFrame.setVisible(true);
+        jFrame.add(wrapperPanel);
     }
 }
