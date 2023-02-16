a351b7653384038b6c46300ff9382a0035c599fc
Joseph Racke <joeracke@gmail.com>
2023-02-16 11:28:33 -0800

    AB#1182 links GUIEntry to titlePanel

--- src/main/java/ui/gui/GUIEntry.java
+++ src/main/java/ui/gui/GUIEntry.java
@@ -12,17 +12,17 @@ import java.io.IOException;
 public class GUIEntry {
     public static void start() throws IOException {
 
-        GlobalVariables.inGameMap = new MainMap(GameDifficulty.Easy);
-        GlobalVariables.gameInitialization();
+//        GlobalVariables.inGameMap = new MainMap(GameDifficulty.Easy);
+//        GlobalVariables.gameInitialization();
 
         // TEST StatsPanel
         JFrame jFrame = new JFrame();
 
-        jFrame.add(new WrapperPanel());
-
+//        jFrame.add(new WrapperPanel());
+        jFrame.add(new TitlePanel());
         jFrame.setResizable(false);
         jFrame.setTitle("StrainX");
-        jFrame.setPreferredSize(new Dimension(1024,768));
+        //jFrame.setPreferredSize(new Dimension(1024,768));
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.pack();
         jFrame.setVisible(true);
