ff31ec8fa5b09b3ff75d8f16a19f068ceb5f5281
antoni <antoni.dev0@gmail.com>
2023-02-17 10:50:02 -0800

    create ConstructHTMLString class and implement parseThreatLevelHTMLString static method. Can now be used in button tool tip hover and in subarea to display threat level, AB#1186

--- src/main/java/ui/gui/components/buttons/SubareaButton.java
+++ src/main/java/ui/gui/components/buttons/SubareaButton.java
@@ -1,6 +1,7 @@
 package ui.gui.components.buttons;
 
 import gamemodel.mapengine.SubArea;
+import ui.gui.ConstructHTMLString;
 import ui.gui.components.panels.SubareaPanel;
 import ui.maps.UIEnterMainMap;
 
@@ -21,71 +22,14 @@ public class SubareaButton extends JButton {
     }
 
     private Integer adjustToolTipText(SubArea subArea){
-        String info = "<html>Threat Level:???<br/>Items Inside:???</html>";
 
-        if(subArea.getVisited()) setToolTipText(parseHTMLString(info,subArea));
-        else defaultToolTipText(info);
+        if(subArea.getVisited()) setToolTipText(ConstructHTMLString.parseThreatLevelHTMLString(subArea));
+        else defaultToolTipText();
 
         return 1;
     }
-    public String parseHTMLString(String str, SubArea subArea){
-        StringBuilder info = new StringBuilder(str);
-
-        int last = str.lastIndexOf("???");
-        int first = str.indexOf("???");
-        info.replace(last,last+3, numberOfItemsInSubarea(subArea));
-        info.replace(first,first+3, colorCodeThreatHTMLSpanTag(subArea));
-
-        return info.toString();
-    }
-    private String numberOfItemsInSubarea(SubArea subArea){
-        String items = "";
-        if(!subArea.getContents().items.isEmpty()){
-            items = String.valueOf(subArea.getContents().items.size());
-        }
-        return items;
-    }
-    private String colorCodeThreatHTMLSpanTag(SubArea subArea){
-        String colorCodeThreat = "<span style='color: black;'>.</span>";
-        String threatLevel = UIEnterMainMap.displayThreatLvl(subArea);
-
-        String txt = "";
-        String color = "";
-
-        if(!convertANSICode(threatLevel).isEmpty()){
-            ArrayList<String> threatInfo = convertANSICode(threatLevel);
-            txt = threatInfo.get(0);
-            color = threatInfo.get(1);
-        }
-
-        colorCodeThreat = colorCodeThreat.replace("black",color);
-        colorCodeThreat = colorCodeThreat.replace(".",txt);
-
-        return colorCodeThreat;
-    }
-    private ArrayList<String> convertANSICode(String ansi){
-        // TODO refactor: convert/parse ANSI code to string
-        ArrayList<String> data = new ArrayList<>();
-        if(ansi.contains("\033[31mHigh\33[0m")){
-            data.add("High");
-            data.add("red");
-        }
-        else if(ansi.contains("\033[33mMedium\33[0m")){
-            data.add("Medium");
-            data.add("orange");
-        }
-        else if(ansi.contains("\033[32mLow\33[0m")){
-            data.add("Low");
-            data.add("green");
-        }
-        else if(ansi.contains("\033[34mSafe\33[0m")){
-            data.add("Safe");
-            data.add("blue");
-        }
-        return data;
-    }
-    private Integer defaultToolTipText(String info){
-        setToolTipText(info);
+    private Integer defaultToolTipText(){
+        setToolTipText(ConstructHTMLString.THREAT_LEVEL_HTML);
         return 1;
     }
     private ActionListener toggleShowSubareaPanel(JPanel subarea, SubArea currArea){
