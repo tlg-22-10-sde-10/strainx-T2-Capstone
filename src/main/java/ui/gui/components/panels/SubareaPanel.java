5d71b393296d70258670e96d66cd86a66ee482be
antoni <antoni.dev0@gmail.com>
2023-02-17 13:36:23 -0800

    a player enters a subarea andthe name,description,threats,and number of items is displayed. AB#1186

--- src/main/java/ui/gui/components/panels/SubareaPanel.java
+++ src/main/java/ui/gui/components/panels/SubareaPanel.java
@@ -1,16 +1,15 @@
 package ui.gui.components.panels;
 
 import gamemodel.mapengine.SubArea;
+import ui.gui.ConstructHTMLString;
 import ui.gui.components.buttons.SubareaButton;
 import ui.maps.UIEnterMainMap;
 
+import javax.accessibility.AccessibleContext;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
+import java.awt.event.*;
 import java.util.Arrays;
 
 public class SubareaPanel extends JPanel {
@@ -23,6 +22,7 @@ public class SubareaPanel extends JPanel {
         setPreferredSize(new Dimension(0,0));
         setVisible(false);
         add(goToMapButton(this));
+        add(goToLootButton(this));
         add(goToCombatButton(this));
         addComponentListener( onSubareaExpand(this) );
     }
@@ -38,7 +38,6 @@ public class SubareaPanel extends JPanel {
             }
         };
     }
-
     private Integer addContainerPanel(SubareaPanel thisPanel){
 
         // TODO set this containers size relative to main map
@@ -46,21 +45,35 @@ public class SubareaPanel extends JPanel {
         int w = thisPanel.getWidth();
         int h = (int) (thisPanel.getHeight()*.90);
 
+        // TODO - TEMP - create Class for this container/subcomponents
         JPanel container = new JPanel();
         container.setPreferredSize(new Dimension(w,h));
         container.setBorder(new LineBorder(Color.BLUE));
-        // Container subPanels
-        container.add(new JLabel(thisPanel.getSubArea().getName()));
-        container.add(new JTextArea(thisPanel.getSubArea().getDescription()));
-        container.add(new JTextArea(String.valueOf(thisPanel.getSubArea().getContents().items.size())));
-        container.add(new JTextArea( UIEnterMainMap.displayThreatLvl(subArea)));
+        container.setLayout(new GridLayout(3,1));
 
-        //
+        // Container sub components
+        JLabel subareaTitleLabel = new JLabel(thisPanel.getSubArea().getName());
+        subareaTitleLabel.setFont(getFont().deriveFont(Font.BOLD,30));
+        subareaTitleLabel.setBorder(new LineBorder(Color.PINK));
+        subareaTitleLabel.setForeground(Color.BLUE);
+
+        JLabel subareaContentLabel = new JLabel(ConstructHTMLString.parseThreatLevelHTMLString(thisPanel.getSubArea()));
+        subareaContentLabel.setBorder(new LineBorder(Color.PINK));
+
+        JTextArea subareaDescriptionTextArea = new JTextArea(thisPanel.getSubArea().getDescription());
+        subareaDescriptionTextArea.setBorder(new LineBorder(Color.PINK));
+
+        // Add Container subPanels
+        container.add(subareaTitleLabel);
+        container.add(subareaContentLabel);
+        container.add(subareaDescriptionTextArea);
+
         thisPanel.add( container );
         thisPanel.revalidate();
         thisPanel.repaint();
         return 1;
     }
+
     private JButton goToMapButton(JPanel subareaPanel){
         JButton btn = new JButton("Map");
         btn.addActionListener( handleReturnToMap(subareaPanel) );
@@ -75,6 +88,12 @@ public class SubareaPanel extends JPanel {
         }
         return btn;
     }
+    private JButton goToLootButton(JPanel subareaPanel){
+        JButton btn = new JButton("Loot");
+        btn.addActionListener(handleGoToLoot());
+        return btn;
+    }
+
     private ActionListener handleGoToCombat(JPanel subareaPanel) {
         return e -> {
             JFrame ancestor = (JFrame) subareaPanel.getTopLevelAncestor();
@@ -93,7 +112,14 @@ public class SubareaPanel extends JPanel {
             ancestor.pack();
         };
     }
-
+    private ActionListener handleGoToLoot(){
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("LOOTING");
+            }
+        };
+    }
 
     public SubArea getSubArea() { return subArea; }
 }
