045e2c6ee2d8c25745e66edc6dff07277739c75d
Joseph Racke <joeracke@gmail.com>
2023-02-20 11:02:42 -0800

    AB#1227 Exit to main menu button applied to setting window

--- src/main/java/ui/gui/components/dialogs/SettingsWindow.java
+++ src/main/java/ui/gui/components/dialogs/SettingsWindow.java
@@ -1,19 +1,22 @@
 package ui.gui.components.dialogs;
 
+import ui.gui.components.buttons.ExitToMainMenuButton;
 import ui.gui.components.buttons.SoundButton;
 import ui.gui.components.buttons.VolumeButton;
 import ui.gui.components.buttons.ExitButton;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class SettingsWindow extends JDialog {
 
+    JFrame parentFrame;
     public SettingsWindow(JFrame frame) {
         super(frame);
+        parentFrame = frame;
+
         setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
 
-        setBounds(200,200,150,150);
+        setBounds(200,200,150,180);
 //        setSize(new Dimension(300,150));
         setResizable(false);
         setLocationRelativeTo(frame);
@@ -22,7 +25,12 @@ public class SettingsWindow extends JDialog {
         add(new SoundButton());
         add(new VolumeButton(1));
         add(new VolumeButton(2));
+        add(new ExitToMainMenuButton());
         add(new ExitButton());
         setVisible(true);
+    }
+
+    public JFrame getParentFrame() {
+        return parentFrame;
     }
 }
