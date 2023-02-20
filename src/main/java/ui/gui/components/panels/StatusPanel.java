0b2787558fa660085a7f85a6087f818cf6b384b1
Lok Tamang <lb.tamang77@gmail.com>
2023-02-20 13:07:03 -0600

    inventory display work in progress, AB#1157

--- src/main/java/ui/gui/components/panels/StatusPanel.java
+++ src/main/java/ui/gui/components/panels/StatusPanel.java
@@ -1,23 +1,56 @@
 package ui.gui.components.panels;
 
 import gamecontrol.contents.CrewMember;
+import ui.gui.components.InventoryDialog;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 public class StatusPanel extends JPanel{
 
 
     public StatusPanel(List<CrewMember> players ){
-        super(new GridLayout(2,2));
+//        super(new GridLayout(2,2));
+        super(new FlowLayout());
+        add(setSubPanelDefaults(players));
+//        displayInventoryButton(this);
 
-        setSubPanelDefaults(players);
+        add(test());
     }
 
-    private Integer setSubPanelDefaults(List<CrewMember> players){
+    private JPanel test(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 1));
+        panel.setBorder(new LineBorder(Color.RED));
+        panel.add(new JButton("Test"));
+        panel.add(new JButton("Test"));
+        panel.add(new JButton("Test"));
+//        panel.setPreferredSize(new Dimension((int)(getParent().getWidth()*.9), getParent().getHeight()));
+        return panel;
+    }
 
+    private void displayInventoryButton(StatusPanel statusPanel) {
+        JButton invButton = new JButton();
+        invButton.setText("Inventory");
+        invButton.setVisible(true);
+        add(invButton);
+        invButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new InventoryDialog((JFrame) statusPanel.getTopLevelAncestor());
+            }
+        });
+    }
+
+    private JPanel setSubPanelDefaults(List<CrewMember> players){
+        JPanel playerContainer = new JPanel(new GridLayout(2, 2));
+//        playerContainer.setPreferredSize(new Dimension((int)(getParent().getWidth()*.9), getParent().getHeight()));
         for (CrewMember crewMember : players) {
+
             JPanel p = new JPanel();
 
             p.setLayout(new FlowLayout());
@@ -28,8 +61,8 @@ public class StatusPanel extends JPanel{
             p.add(label);
 
             p.setBackground(Color.GRAY);
-            add(p);
+            playerContainer.add(p);
         }
-        return 1;
+        return playerContainer;
     }
 }
