2f12d1e9929a2f2af8276c2911861d51e4ea74e6
Joseph Racke <joeracke@gmail.com>
2023-02-24 09:00:51 -0800

    AB#1389 Music now stops on game over (win or lose)

--- src/main/java/ui/gui/components/JOptionPanes.java
+++ src/main/java/ui/gui/components/JOptionPanes.java
@@ -2,6 +2,7 @@ package ui.gui.components;
 
 import gamecontrol.GlobalVariables;
 import gamemodel.combatengine.EngageEnemy;
+import gamemusic.AudioPlayer;
 import ui.gui.components.panels.TitlePanel;
 
 import javax.swing.*;
@@ -13,6 +14,7 @@ public class JOptionPanes {
 
     public static boolean youLosePane(JFrame frame) {
         if(GlobalVariables.mySquad.get(0).getHP() <= 0) {
+            if (AudioPlayer.isSoundOn()) AudioPlayer.getInstance().stopAudio();
             if (GUISoundEffects.isSoundOn()) GUISoundEffects.playSound("sound/game-over.wav");
             int i = JOptionPane.showConfirmDialog(frame,
                     "You died! Do you want to play again?",
@@ -34,6 +36,7 @@ public class JOptionPanes {
 
     public static boolean youWinPane(JFrame frame) {
         if(EngageEnemy.getEnemyKIAList().stream().anyMatch(ex -> ex.getName().equals(FINAL_BOSS.getName()))) {
+            if (AudioPlayer.isSoundOn()) AudioPlayer.getInstance().stopAudio();
             if (GUISoundEffects.isSoundOn()) GUISoundEffects.playSound("sound/victory.wav");
             int i = JOptionPane.showConfirmDialog(frame,
                     "You won! You found and defeated Patient Zero!\nDo you want to play again?",
